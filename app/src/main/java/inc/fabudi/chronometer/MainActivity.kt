adbc9db3d251b74661eef6a892fd96e233433189
Fabudi <evgeniydodg@gmail.com>
2023-06-15 08:06:10 +0300

    - Show Toast every 10 seconds in second thread  - Show "Surprise" Toast every 4 second thread execution

--- app/src/main/java/inc/fabudi/chronometer/MainActivity.kt
+++ app/src/main/java/inc/fabudi/chronometer/MainActivity.kt
@@ -3,6 +3,7 @@ package inc.fabudi.chronometer
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -19,6 +20,8 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
 
     companion object {
         var counter = 0
+        var surprise = false
+        var secondCoroutineCounter = 0
     }
 
     @SuppressLint("SetTextI18n")
@@ -28,11 +31,32 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
         label = findViewById(R.id.label)
         scope.launch(Dispatchers.IO) {
             while (true) {
-                delay(1000)
                 withContext(Dispatchers.Main) {
-                    label.text = (++counter).toString()
+                    label.text = (counter).toString()
                 }
+                ++counter
+                delay(1000)
             }
         }
+        scope.launch(Dispatchers.IO) {
+            while (true) {
+                withContext(Dispatchers.Main) {
+                    showMessage()
+                }
+                delay(10000)
+                ++secondCoroutineCounter
+            }
+        }
+        scope.launch(Dispatchers.IO) {
+            while (true) {
+                surprise = secondCoroutineCounter % 4 == 0
+            }
+        }
+    }
+
+    private fun showMessage() {
+        Toast.makeText(
+            this@MainActivity, if (surprise) "Surprise" else counter.toString(), Toast.LENGTH_SHORT
+        ).show()
     }
 }
